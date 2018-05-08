82db26c45e2f57c52eb5814a5620044e1085359f
Patrick Weißkirchner <patrick.weiszkirchner@gmail.com>
2018-05-08 13:24:40 +0200

    Copy resource file to tmp directory for file loading

--- src/main/java/at/ac/tuwien/otl/ss18/pwlk/verifier/SolutionVerifier.java
+++ src/main/java/at/ac/tuwien/otl/ss18/pwlk/verifier/SolutionVerifier.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class SolutionVerifier {
     private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -11,15 +14,28 @@ public class SolutionVerifier {
     private String jarPath;
 
     public SolutionVerifier() {
-        this.jarPath = this.getClass().getClassLoader().getResource("verifier/evrptw-verifier-0.2.0.jar.verify").getPath();
+        try {
+            InputStream verifierExecutable = this.getClass().getClassLoader().getResourceAsStream("verifier/evrptw-verifier-0.2.0.jar.verify");
+            Path jarDestination = File.createTempFile("verifier", ".jar").toPath();
+            Files.copy(verifierExecutable, jarDestination, StandardCopyOption.REPLACE_EXISTING);
+
+            jarPath = jarDestination.toString();
+
+        } catch (IOException i) {
+            logger.error("Exception occured while loading verifier: " + i);
+        }
     }
 
     public void verify(String pathToProblem, String pathToSolution) {
         try {
+            InputStream problemInstance = this.getClass().getClassLoader().getResourceAsStream(pathToProblem);
+            Path problemDestination = File.createTempFile("problem", ".txt").toPath();
+            Files.copy(problemInstance, problemDestination, StandardCopyOption.REPLACE_EXISTING);
+
             Process proc = Runtime.getRuntime().exec("java -jar "
                     + jarPath
                     + " "
-                    + this.getClass().getClassLoader().getResource(pathToProblem).getFile()
+                    + problemDestination.toString()
                     + " "
                     + pathToSolution);
 
