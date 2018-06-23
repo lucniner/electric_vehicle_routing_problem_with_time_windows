8a7b4c949110f4d746ae943d8c37b6b1adde4736
Lukas Kathrein <lukas.kathrein@gmx.net>
2018-06-23 16:45:13 +0200

    implemented two and three opt exchanges

--- src/main/java/at/ac/tuwien/otl/ss18/pwlk/metaHeuristics/impl/BestOrOptExchange.java
+++ src/main/java/at/ac/tuwien/otl/ss18/pwlk/metaHeuristics/impl/BestOrOptExchange.java
@@ -28,6 +28,18 @@ public class BestOrOptExchange {
 
     public Optional<Route> optimizeRoute() {
 
+        singleOptExchange();
+        twoOptExchange();
+        threeOptExchange();
+
+        if (routes.peek() != null) {
+            return Optional.of(routes.peek());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private void singleOptExchange() {
         //starting at one because of depot and also ignoring depot at the end
         for (int i = 1; i < route.getRoute().size() - 1; i++) {
             for (int j = 1; j < route.getRoute().size() - 1; j++) {
@@ -36,26 +48,54 @@ public class BestOrOptExchange {
 
                 final AbstractNode exchangeNode = nodes.remove(i);
                 nodes.add(j, exchangeNode);
-
-                Car car = new Car(problemInstance, distanceHolder);
-                try {
-                    car.driveRoute(nodes);
-                    opt.setDistance(car.getCurrentDistance());
-
-                    if (opt.getDistance() < route.getDistance()) {
-                        routes.add(opt);
-                    }
-                } catch (BatteryViolationException | TimewindowViolationException e) {
-
-                }
+                driveCar(nodes, opt);
             }
         }
+    }
+
+    private void twoOptExchange() {
+        for (int i = 1; i < route.getRoute().size() - 2; i++) {
+            for (int j = 1; j < route.getRoute().size() - 2; j++) {
+                final Route opt = route.copyRoute();
+                final List<AbstractNode> nodes = opt.getRoute();
+                final AbstractNode exchangeNode = nodes.remove(i);
+                final AbstractNode nextExchange = nodes.remove(i);
+                nodes.add(j, exchangeNode);
+                nodes.add(j + 1, nextExchange);
+                driveCar(nodes, opt);
+            }
+        }
+    }
+
+    private void threeOptExchange() {
+        for (int i = 1; i < route.getRoute().size() - 3; i++) {
+            for (int j = 1; j < route.getRoute().size() - 3; j++) {
+                final Route opt = route.copyRoute();
+                final List<AbstractNode> nodes = opt.getRoute();
+
+                final AbstractNode exchangeNode = nodes.remove(i);
+                final AbstractNode nextExchange = nodes.remove(i);
+                final AbstractNode furtherExchange = nodes.remove(i);
+                nodes.add(j, exchangeNode);
+                nodes.add(j + 1, nextExchange);
+                nodes.add(j + 2, furtherExchange);
+                driveCar(nodes, opt);
+            }
+        }
+    }
 
 
-        if (routes.peek() != null) {
-            return Optional.of(routes.peek());
-        } else {
-            return Optional.empty();
+    private void driveCar(final List<AbstractNode> nodes, final Route opt) {
+        Car car = new Car(problemInstance, distanceHolder);
+        try {
+            car.driveRoute(nodes);
+            opt.setDistance(car.getCurrentDistance());
+
+            if (opt.getDistance() < route.getDistance()) {
+                routes.add(opt);
+            }
+        } catch (BatteryViolationException | TimewindowViolationException e) {
+
         }
     }
 }
